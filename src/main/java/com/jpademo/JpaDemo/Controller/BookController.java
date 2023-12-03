a831a88bf5abf26f6848573d5d4b0455328a10ff
vivekbhardwaj17 <vivekofficial2018@gmail.com>
2023-12-03 21:06:19 +0530

    added missed import

--- src/main/java/com/jpademo/JpaDemo/Controller/BookController.java
+++ src/main/java/com/jpademo/JpaDemo/Controller/BookController.java
@@ -4,37 +4,38 @@ import com.jpademo.JpaDemo.DAO.BookDAO;
 import com.jpademo.JpaDemo.DAO.BookDAOImple;
 import com.jpademo.JpaDemo.Entity.Books;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-    @RequestMapping("/book/v1")
+@RequestMapping("/book/v1")
 public class BookController {
 
     private BookDAO bookDAO;
 
     @Autowired
     public BookController(BookDAO thebookDAO) {
-        bookDAO=thebookDAO;
+        bookDAO = thebookDAO;
     }
 
     @GetMapping("/savedata")
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         System.out.println("Creating the Book");
         //create student
-        Books tempBook =new Books(2,"Tenth of December","George Saunders",2800.00);
+        Books tempBook = new Books(2, "Tenth of December", "George Saunders", 2800.00);
 
         System.out.println("Saving the book");
         bookDAO.save(tempBook);
         return "success";
     }
-     @PostMapping("/savedata")
-    public String getDailyWorkout(@RequestBody Books book){
-         if(book!=null)
-           bookDAO.save(book);
-        return "success";
+
+    @PostMapping("/savedata")
+    public ResponseEntity getDailyWorkout(@RequestBody Books book) {
+        if (book != null)
+            bookDAO.save(book);
+        return new ResponseEntity(book, HttpStatusCode.valueOf(201));
     }
 
 }
